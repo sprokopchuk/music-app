ca4932822623f74dabe79be481573f47ed7059ef
Sergey Prokopchuk <prokopchuksv21@gmail.com>
2020-02-16 22:08:01 +0200

    Fix Spotify autorization

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,22 @@ import SearchBar from './SearchBar';
 import spotify from './api/spotify';
 
 class App extends React.Component {
+  state = { tracks: [] }
+
+  componentDidMount() {
+    spotify.get('/search', {
+      params: {
+        q: 'muse',
+        type: 'track'
+      }
+    }).then((response) => {
+      if (response.status === 200) {
+        this.setState( { tracks: response.data.tracks.items })
+        console.log(response.data.tracks.items)
+      }
+    });
+  }
+
   render() {
     return (
       <div className='ui container'>
--- src/components/api/spotify.js
+++ src/components/api/spotify.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { getFromLocalStorage } from '../utils'
+import { getFromLocalStorage, saveToLocalStorage } from '../utils'
 
 const CLIENT_ID = 'dd8c0d9d0bc1413eb4294d6cd058b0d2';
 const CLIENT_SECRET = '7442b20b2abd4a66967e61fce8ff0468';
 const AUTHORIZATION_TOKEN = 'spotify_token';
 
-const authorize = () => {
-  return axios({
+const authorize = async () => {
+  const response = await axios({
     url: 'https://accounts.spotify.com/api/token',
     method: 'POST',
     headers: {
@@ -14,13 +14,19 @@ const authorize = () => {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     data: 'grant_type=client_credentials'
-  })
+  });
+
+  if(response.status === 200) {
+    saveToLocalStorage(AUTHORIZATION_TOKEN, response.data.access_token)
+    return response.data.access_token;
+  } else {
+    return '';
+  }
 };
 
 const getToken = () => {
-  return getFromLocalStorage(AUTHORIZATION_TOKEN);
+  return getFromLocalStorage(AUTHORIZATION_TOKEN) || authorize();
 };
-
 
 export default axios.create({
   baseURL: 'https://api.spotify.com/v1/',
