8bd74bc69bd1bee01f147f438b48dbceb0add8c0
Sergey Prokopchuk <prokopchuksv21@gmail.com>
2020-02-17 22:57:21 +0200

    Add audio to track

--- src/components/TrackItem.js
+++ src/components/TrackItem.js
@@ -6,8 +6,18 @@ const TrackItem = ({ track }) => {
     <div className='item track-item'>
       <i className="music icon"></i>
       <div className='content'>
-        <div className='header'>{track.name}</div>
-        <div className='description'>{track.artists.map(artist => artist.name).join(', ')}</div>
+        <div className='ui grid'>
+          <div className="eight wide column">
+            <div className='header'>{track.name}</div>
+            <div className='description'>{track.artists.map(artist => artist.name).join(', ')}</div>
+          </div>
+          <div className="eight wide column">
+            <audio
+                controls
+                src={track.preview_url}>
+            </audio>
+          </div>
+        </div>
       </div>
     </div>
   );
