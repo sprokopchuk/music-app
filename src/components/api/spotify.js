d6e5aeef6f947ba57973d8df067a774c58735d89
Sergey Prokopchuk <prokopchuksv21@gmail.com>
2020-02-26 20:36:37 +0200

    Fix authorization logic

--- src/components/api/spotify.js
+++ src/components/api/spotify.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { getFromLocalStorage, saveToLocalStorage } from '../utils'
+import Cookies from 'js-cookie'
 
 const CLIENT_ID = 'dd8c0d9d0bc1413eb4294d6cd058b0d2';
 const CLIENT_SECRET = '7442b20b2abd4a66967e61fce8ff0468';
 const AUTHORIZATION_TOKEN = 'spotify_token';
 
-const authorize = async () => {
-  const response = await axios({
+const authorize = () => {
+  axios({
     url: 'https://accounts.spotify.com/api/token',
     method: 'POST',
     headers: {
@@ -14,18 +14,19 @@ const authorize = async () => {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     data: 'grant_type=client_credentials'
+  }).then((response) => {
+    if(response.status === 200) {
+      const inOneHour = new Date(new Date().getTime() + response.data.expires_in * 1000);
+      Cookies.set(AUTHORIZATION_TOKEN, response.data.access_token, { expires: inOneHour });
+    } else {
+      console.log('There is something wrong with authorization');
+    }
   });
-
-  if(response.status === 200) {
-    saveToLocalStorage(AUTHORIZATION_TOKEN, response.data.access_token)
-    return response.data.access_token;
-  } else {
-    return '';
-  }
+  return Cookies.get(AUTHORIZATION_TOKEN);
 };
 
 const getToken = () => {
-  return getFromLocalStorage(AUTHORIZATION_TOKEN) || authorize();
+  return Cookies.get(AUTHORIZATION_TOKEN) || authorize();
 };
 
 export default axios.create({
