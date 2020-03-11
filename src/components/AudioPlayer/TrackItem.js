4da104fa2849baeb7586b35e4e0dd8c367cf1791
Sergey Prokopchuk <prokopchuksv21@gmail.com>
2020-03-11 21:31:24 +0200

    Make TrackItem functional component

--- src/components/AudioPlayer/TrackItem.js
+++ src/components/AudioPlayer/TrackItem.js
@@ -2,26 +2,23 @@ import React from 'react';
 import cn from 'classnames';
 import '../../css/track_item.scss'
 
-class TrackItem extends React.Component {
-  render () {
-    const { track, onTrackClick, isPlaying, isSelected } = this.props;
-    return (
-      <div className='item track-item'>
-        <i
-          className={cn('icon', {
-            disabled: !track.preview_url,
-            pause: isPlaying && isSelected,
-            play: !isPlaying && isSelected
-          })}
-          onClick={onTrackClick}
-        />
-        <div className='content'>
-          <div className='header'>{track.name}</div>
-          <div className='description'>{track.artists.map(artist => artist.name).join(', ')}</div>
-        </div>
+const TrackItem = ({ track, onTrackClick, isPlaying, isSelected }) => {
+  return (
+    <div className='item track-item'>
+      <i
+        className={cn('icon', {
+          disabled: !track.preview_url,
+          pause: isPlaying && isSelected,
+          play: !isPlaying && isSelected
+        })}
+        onClick={onTrackClick}
+      />
+      <div className='content'>
+        <div className='header'>{track.name}</div>
+        <div className='description'>{track.artists.map(artist => artist.name).join(', ')}</div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default TrackItem;
