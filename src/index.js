ba54415ed68d420cf6617baca3c5daffff71796d
Sergey Prokopchuk <prokopchuksv21@gmail.com>
2020-03-12 22:50:30 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import reducers from './reducers';
 import App from './components/App';
 
 const sagaMiddleware = createSagaMiddleware();
-const composeEnhancers = global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleware, audioMiddleware)))
+const composeEnhancers = global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleware, audioMiddleware)));
 
 sagaMiddleware.run(sagas);
 
