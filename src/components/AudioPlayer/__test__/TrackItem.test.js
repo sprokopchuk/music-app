a0771390c67e727264b8133556e421cf4903918c
Sergey Prokopchuk <prokopchuksv21@gmail.com>
2020-03-31 16:51:21 +0300

    Add test for TrackItem component

--- src/components/AudioPlayer/__test__/TrackItem.test.js
+++ src/components/AudioPlayer/__test__/TrackItem.test.js
@@ -1,45 +1,45 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 import TrackItem from '../TrackItem';
 
 
 describe('<TrackItem />', () => {
   it('renders pause icon', () => {
     const props = {
-      track: { id: 1, preview_url: 'audio_url', artists: ['Bob', 'Joe'] },
+      track: { id: 1, preview_url: 'audio_url', artists: [{ name: 'Bob' }, { name: 'Joe' }] },
       onTrackClick: jest.fn(),
       isPlaying: true,
       isSelected: true
     };
 
-    const wrapper = render(<TrackItem {...props} />);
+    const wrapper = shallow(<TrackItem {...props} />);
 
-    expect(wrapper.find('.pause')).to.have.lengthOf(1);
+    expect(wrapper.find('.icon').hasClass('pause')).toBe(true)
   });
 
   it('renders play icon', () => {
     const props = {
-      track: { id: 1, preview_url: 'audio_url', artists: ['Bob', 'Joe'] },
+      track: { id: 1, preview_url: 'audio_url', artists: [{ name: 'Bob' }, { name: 'Joe' }] },
       onTrackClick: jest.fn(),
       isPlaying: false,
       isSelected: true
     };
 
-    const wrapper = render(<TrackItem {...props} />);
+    const wrapper = shallow(<TrackItem {...props} />);
 
-    expect(wrapper.find('.pause')).to.have.lengthOf(1);
+    expect(wrapper.find('.icon').hasClass('play')).toBe(true)
   });
 
   it('renders disabled icon', () => {
     const props = {
-      track: { id: 1, preview_url: null, artists: ['Bob', 'Joe'] },
+      track: { id: 1, preview_url: null, artists: [{ name: 'Bob' }, { name: 'Joe' }] },
       onTrackClick: jest.fn(),
       isPlaying: false,
       isSelected: false
     };
 
-    const wrapper = render(<TrackItem {...props} />);
+    const wrapper = shallow(<TrackItem {...props} />);
 
-    expect(wrapper.find('.pause')).to.have.lengthOf(1);
+    expect(wrapper.find('.icon').hasClass('disabled')).toBe(true)
   });
 });
